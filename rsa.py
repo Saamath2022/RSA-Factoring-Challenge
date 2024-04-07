53ce62da8b0cb80f1a985b2ab1fbc6d179421fa5
Saamath2023 <saaaondow@gmail.com>
2024-04-07 12:23:23 -0700

    rsa

--- rsa.py
+++ rsa.py
@@ -1,38 +1,18 @@
 #!/usr/bin/python3
-import sys
-import random
+from sys import argv
 
-
-def miller_rabin(n, k=5):
-    if n == 2 or n == 3:
-        return True
-    if n < 2 or n % 2 == 0:
-        return False
-
-    r, d = 0, n - 1
-    while d % 2 == 0:
-        r += 1
-        d //= 2
-    for _ in range(k):
-        a = random.randint(2, n - 2)
-        x = pow(a, d, n)
-
-        if x == 1 or x == n - 1:
+with open(argv[1]) as f:
+    for line in f:
+        num = int(line)
+        print("{:d}m".format(num), end='')
+        if num % 2 == 0:
+            print("{} * 2".format(num//2))
             continue
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == -1:
+        for i in range(3, num, 2):
+            if num % i == 0:
+                factor = num // i
+                for j in range(3, factor, 2):
+                    if factor % j == 0 or j == 0:
+                        break
+                print("{} * {}".format(factor, i))
                 break
-        else:
-            return False
-    return True
-
-
-def is_prime(n):
-    return miller_rabin(n)
-
-
-def factorize_rsa(n):
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0 and is_prime(i) and is_prime(n // i):
-            return i, n // i
